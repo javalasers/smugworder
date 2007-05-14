c9d9786a23cad916f0ad067f6c57745a87e503ef
willrobinson <willrobinson@dbfaa404-6430-0410-aa45-9b677763089b>
2007-05-14 05:03:36 +0000

    Initial checkin of smugworder.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='smugworder',
+      version='1.0',
+      description='smugworder',
+      author='Will Robinson',
+      author_email='willrobinson@gmail.com',
+      url='http://
+      packages=['smugworder', 'http://code.google.com/p/smugworder/'],
+      )
